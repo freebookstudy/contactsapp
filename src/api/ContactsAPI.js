cd48c1be69af805918448168b6c9db017061ad8d
freelife1191 <freelife1191.good@gmail.com>
2018-01-08 18:08:18 +0900

    최초 커밋

---
+++ src/api/ContactsAPI.js
@@ -0,0 +1,42 @@
+import CONF from '../Config.js';
+import axios from 'axios';
+
+export default {
+    fetchContacts : function(pageno, pagesize) {
+        console.log(CONF.FETCH);
+        return axios.get(CONF.FETCH, {
+            params : { 
+                pageno: pageno, 
+                pagesize: pagesize 
+            } 
+        })
+    },
+    fetchContactOne : function(no) {
+        return axios.get(CONF.FETCH_ONE.replace("${no}", no))
+    },
+    addContact : function(contact) {
+        return axios.post(CONF.ADD,  contact);
+    },
+    updateContact : function(contact) {
+        return axios.put(CONF.UPDATE.replace("${no}", contact.no), contact)
+    },
+    deleteContact : function(no) {
+        return axios.delete(CONF.DELETE.replace("${no}", no))
+    },
+    updatePhoto : function(no, file) {
+        var data = new FormData();
+        data.append('photo', file);
+
+        return axios.post(CONF.UPDATE_PHOTO.replace("${no}", no), data)
+    },
+
+    fetchMngrsts : function(pageno, pagesize) {
+        console.log(CONF.FETCH_MNG);
+        return axios.get(CONF.FETCH_MNG, {
+            params : { 
+                pageno: pageno, 
+                pagesize: pagesize 
+            } 
+        })
+    },
+}
